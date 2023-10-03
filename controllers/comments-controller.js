5f73c593e9e3f655d57b91d2964df18bbb17511c
Sir1ys <sashakoroviy@gmail.com>
2023-10-03 16:16:26 +0100

    first test was passed, model and controller for comments were set up

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,5 +1,10 @@
 const { fetchComments } = require("../models/comments-model");
 
 exports.getComments = (req, res, next) => {
-  fetchComments();
+  const { article_id } = req.params;
+  fetchComments(article_id)
+    .then(({ comments }) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => next(err));
 };
--- models/comments-model.js
+++ models/comments-model.js
@@ -1,1 +1,13 @@
-exports.fetchComments = () => {}
+const db = require("../db/connection");
+
+exports.fetchComments = (article_id) => {
+  const query = `SELECT comment_id, votes, created_at, author, body, article_id 
+  FROM comments  
+  WHERE article_id = $1;`;
+
+  return db.query(query, [article_id]).then(({ rows }) => {
+    return {
+      comments: rows,
+    };
+  });
+};
