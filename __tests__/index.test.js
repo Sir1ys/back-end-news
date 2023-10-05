c6a51901074cf9bef98c8dd0dcca1dcda8e53393
Sir1ys <sashakoroviy@gmail.com>
2023-10-05 12:19:43 +0100

    GET article: test was changed

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -82,13 +82,6 @@ describe("/api/articles/:article_id", () => {
         expect(article).toEqual(
           expect.objectContaining({
             article_id: 1,
-            author: expect.any(String),
-            title: expect.any(String),
-            body: expect.any(String),
-            topic: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number),
-            article_img_url: expect.any(String),
             comment_count: "11",
           })
         );
