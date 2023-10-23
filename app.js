d66dd3e7cc7d27fe05309b6dcee73c945aec2611
Sir1ys <sashakoroviy@gmail.com>
2023-10-23 14:18:27 +0100

    cors problem was fixed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const cors = require('cors');
+const express = require("express");
 
 const {
   handleCustomErrors,
@@ -8,12 +8,12 @@ const {
   handleWrongUrlErrors,
 } = require("./errors");
 
-app.use(cors());
 
 const apiRouter = require("./routers/api-router");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
